22d5147f7e2d7ad5df7f25d7c7aac9ceaba83817
Sohanur Rahman <sohanasif@gmail.com>
2024-08-18 14:18:56 +0600

    add libraries

--- android/app/build/generated/autolinking/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/autolinking/src/main/java/com/facebook/react/PackageList.java
@@ -10,7 +10,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
 public class PackageList {
   private Application application;
@@ -55,7 +56,8 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig)
+      new MainReactPackage(mConfig),
+      new AsyncStoragePackage()
     ));
   }
 }
